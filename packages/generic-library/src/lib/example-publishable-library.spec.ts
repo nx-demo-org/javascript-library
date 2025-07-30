ff47e797d5a26d3ea0b4c45bc907a05573fc6447
Josh VanAllen <5290334+joshvanallen@users.noreply.github.com>
2025-07-30 16:49:56 -0400

    breaking change

--- packages/generic-library/src/lib/example-publishable-library.spec.ts
+++ packages/generic-library/src/lib/example-publishable-library.spec.ts
@@ -3,5 +3,6 @@ import { examplePublishableLibrary } from './example-publishable-library.js';
 describe('examplePublishableLibrary', () => {
   it('should work', () => {
     expect(examplePublishableLibrary()).toEqual('example-publishable-library');
+    expect(true).toEqual(false);
   });
 });
